b7e9f38b2969fcfb6f1522dfafb4d7315e32fc33
HebhAlawam <yaheaawam3@gmail.com>
2026-02-03 23:13:17 +0300

    fix frontend

---
+++ postcss.config.js
@@ -0,0 +1,31 @@
+// import purgecss from '@fullhuman/postcss-purgecss'
+
+// export default {
+//   plugins: [
+//     purgecss({
+//       // هنا نخبر الأداة أن تبحث في كل ملفات الـ HTML في المجلد الرئيسي 
+//       // وفي أي ملفات JS داخل مجلد js
+//       content: ['./*.html', './js/**/*.js'], 
+      
+//       // هذه القائمة تحمي الكلاسات التي يضيفها البوتستراب أو الـ Plugins برمجياً
+//       safelist: {
+//         standard: ['show', 'fade', 'active'],
+//         greedy: [/nav-/, /modal-/, /dropdown-/] 
+//       }
+//     })
+//   ]
+// }
+
+
+
+
+
+
+
+// module.exports = {
+//   plugins: [
+//     require('purgecss')({
+//       content: ['./index.html', './**/*.js', './**/*.vue'], // مسارات الملفات التي تحتوي على HTML أو JS أو Vue
+//     }),
+//   ],
+// };
